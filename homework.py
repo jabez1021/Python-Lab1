90f23f551035076c792e843513d8f696b4111744
jabez1021 <jabez1021@hotmail.com>
2026-05-14 22:28:00 +0900

    과제 작성해보기

---
+++ homework.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+try:
+    driver.get('https://finance.naver.com/sise/')
+    elem = driver.find_element(By.ID, 'popularItemList')
+    childs = elem.find_elements(By.TAG_NAME, 'li')
+
+    for child in childs:
+        print(child.text)
+except Exception as e:
+    print(e)
+finally:
+    driver.quit()
